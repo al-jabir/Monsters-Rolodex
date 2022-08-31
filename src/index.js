8466da1f9991130ad5cd1800745b94ec4f8db02f
MD AL JABIR <aljabir098@gmail.com>
2022-09-01 01:31:12 +0600

    first project with reactJS

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 // import Counter from "./components/Counter";
-import "./index.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import './index.css';
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <App />
